7b66a6a796ac33817f6dacbd0b4c46dd28c23de3
jatgm <37679610+jatgm@users.noreply.github.com>
2021-03-22 07:58:28 -0700

    Remove comments
    Not needed

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 # Defining the home page of our site
-@app.route("/home")  # this sets the route to this page
+@app.route("/home")  
 def home():
 	return render_template("index.html")
 
-@app.route("/pricing")  # this sets the route to this page
+@app.route("/pricing") 
 def pricing():
 	return render_template("pricing.html")
 
-@app.route("/about")  # this sets the route to this page
+@app.route("/about")  
 def about():
 	return render_template("about.html")
 
